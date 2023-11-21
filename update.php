db85f20c28a341725ac40c9873f859cfbef05d08
nguyenthanh <150878788+Nguyen-thanh844@users.noreply.github.com>
2023-11-21 07:27:51 +0700

    Add files via upload
    quản lí danh mục

---
+++ update.php
@@ -0,0 +1,55 @@
+<div class="pcoded-content">
+
+    <div class="page-header card">
+        <div class="row align-items-end">
+            <div class="col-lg-8">
+                <div class="page-header-title">
+                    <i class="feather icon-home bg-c-blue"></i>
+                    <div class="d-inline">
+                        <h5>Category Update</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="page-header-breadcrumb">
+                    <ul class=" breadcrumb breadcrumb-title">
+                        <li class="breadcrumb-item">
+                            <a href="/addmin/dashboard"><i class="feather icon-home"></i></a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="#!">Category Update</a> </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pcoded-inner-content">
+        <div class="main-body">
+            <div class="page-wrapper">
+                <div class="page-body">
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h5>Cập nhật </h5>
+                                </div>
+                                <div class="card-block">
+                                    <form action="" method="post">
+                                        <label for="name">Name</label>
+                                        <input type="text" name="name_categories" class="form-control" value="<?= $category['name_categories'] ?>">
+                                
+                                        <button type="submit" name="btn-submit" class="btn btn-info mt-3">Submit</button>
+                                        <a href="/admin/categories" class="btn btn-primary mt-3">Quay lại d/s</a>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+</div>
